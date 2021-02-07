6cb28630802f640fa5a6c3f6c1be4a9905fab82b
oniichan-cpu <fabianmann9@gmail.com>
2021-02-07 16:34:30 +0545

    nyah

--- new/main.py
+++
@@ -1,7 +0,0 @@
-from data import virtualgame
-
-virtualgame = virtualgame.RedditGame()
-
-virtualgame.makePortofolio("https://www.reddit.com/r/NepalStock/comments/iwzh0n/weekly_trading_game_test/")
-
-virtualgame.calculate()
